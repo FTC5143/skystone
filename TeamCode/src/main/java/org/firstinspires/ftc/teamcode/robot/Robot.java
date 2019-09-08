3e6ca8ba4f3bdfafd782a3d0f6c876bab52e071c
qwerty <qwertytrogi@gmail.com>
2019-09-08 14:47:28 -0500

    Begin basic core code structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+    public Robot(HardwareMap hardwareMap) {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/Component.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.robot.components;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Component {
+    public Component(HardwareMap hardwareMap) {
+        registerHardware(hardwareMap);
+    }
+
+    private void registerHardware(HardwareMap hardwareMap) {
+
+    }
+}
