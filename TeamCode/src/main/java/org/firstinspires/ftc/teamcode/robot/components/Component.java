28fe7cf9c17fa51851aabe1817a1613f7a757443
epicc_haxor69 <john.abolins@beasaint.org>
2019-10-10 16:10:50 -0500

    Revert "Revert "optimizing the""
    This reverts commit 1ce4cac4ceeb760f6f87131f97c361620a3ac7a8.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/Component.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/Component.java
@@ -2,9 +2,8 @@ package org.firstinspires.ftc.teamcode.robot.components;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.robot.robots.Robot;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.text.DecimalFormat;
 
