d4888ac32aea8df108dfdedf467b6eb579a120c2
robotics <robotics@xaviersaints.org>
2019-10-24 10:05:38 -0500

    Switch to DogeCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/debug/PhoneCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/debug/PhoneCamera.java
@@ -3,31 +3,22 @@ package org.firstinspires.ftc.teamcode.robot.components.debug;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.robot.components.Component;
 import org.firstinspires.ftc.teamcode.robot.robots.Robot;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
+import java.util.Locale;
+
+import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
-import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.util.Arrays;
 
 public class PhoneCamera extends Component {
 
-    OpenCvCamera phone_camera;
-
-    SamplePipeline stone_pipeline;
+    private OpenCvCamera phone_camera;
+    private SkystoneDetector skystone_detector;
 
     public PhoneCamera(Robot robot) {
         super(robot);
@@ -45,23 +36,22 @@ public class PhoneCamera extends Component {
 
         phone_camera.openCameraDevice();
 
-        stone_pipeline = new SamplePipeline();
-        phone_camera.setPipeline(stone_pipeline);
+        skystone_detector = new SkystoneDetector();
+        phone_camera.setPipeline(skystone_detector);
     }
 
     @Override
     protected void updateTelemetry(Telemetry telemetry) {
         super.updateTelemetry(telemetry);
 
+        telemetry.addData("SP X", skystone_detector.getScreenPosition().x);
+        telemetry.addData("SP Y", skystone_detector.getScreenPosition().y);
         telemetry.addData("FRAME", phone_camera.getFrameCount());
-        telemetry.addData("FPS", String.format("%.2f", phone_camera.getFps()));
-        telemetry.addData("TFT MS", phone_camera.getTotalFrameTimeMs());
+        telemetry.addData("FPS", String.format(Locale.US, "%.2f", phone_camera.getFps()));
+        telemetry.addData("TF MS", phone_camera.getTotalFrameTimeMs());
         telemetry.addData("PT MS", phone_camera.getPipelineTimeMs());
         telemetry.addData("OT MS", phone_camera.getOverheadTimeMs());
         telemetry.addData("MAX FPS", phone_camera.getCurrentPipelineMaxFps());
-        telemetry.addData("LEFT RECT", stone_pipeline.left_hue+" "+stone_pipeline.left_br);
-        telemetry.addData("RIGHT RECT", stone_pipeline.right_hue+" "+stone_pipeline.right_br);
-
     }
 
     @Override
@@ -80,101 +70,5 @@ public class PhoneCamera extends Component {
 
     public void stop_streaming() {
         phone_camera.stopStreaming();
-    }
-
-    class SamplePipeline extends OpenCvPipeline
-    {
-        int left_hue;
-        int right_hue;
-
-        int left_br;
-        int right_br;
-
-        @Override
-        public Mat processFrame(Mat input)
-        {
-            int[] left_rect = {
-                    (int)(input.cols()*(1f/4f)),
-                    (int)(input.cols()*(1f/8f)),
-                    (int)(input.cols()*(2f/4f)),
-                    (int)(input.cols()*(1f/4f))
-            };
-
-            int[] right_rect = {
-                    (int)(input.cols()*(2f/4f)),
-                    (int)(input.cols()*(1f/8f)),
-                    (int)(input.cols()*(3f/4f)),
-                    (int)(input.cols()*(1f/4f))
-            };
-
-            Imgproc.rectangle(
-                    input,
-                    new Point(
-                            left_rect[0],
-                            left_rect[1]),
-
-                    new Point(
-                            left_rect[2],
-                            left_rect[3]),
-                    new Scalar(0, 255, 0), 2);
-
-            Imgproc.rectangle(
-                    input,
-                    new Point(
-                            right_rect[0],
-                            right_rect[1]),
-
-                    new Point(
-                            right_rect[2],
-                            right_rect[3]),
-                    new Scalar(0, 0, 255), 2);
-
-            Mat left_block = input.submat(left_rect[1], left_rect[3], left_rect[0], left_rect[2]);
-            Mat right_block = input.submat(right_rect[1], right_rect[3], right_rect[0], right_rect[2]);
-
-
-            Scalar left_mean = Core.mean(left_block);
-
-
-            Scalar right_mean = Core.mean(right_block);
-
-            left_hue = get_hue((int) left_mean.val[0], (int) left_mean.val[1], (int) left_mean.val[2]);
-            right_hue = get_hue((int) right_mean.val[0], (int) right_mean.val[1], (int) right_mean.val[2]);
-            left_br = get_brightness((int) left_mean.val[0], (int) left_mean.val[1], (int) left_mean.val[2]);
-            right_br = get_brightness((int) right_mean.val[0], (int) right_mean.val[1], (int) right_mean.val[2]);
-
-            return input;
-        }
-    }
-
-
-    private int get_hue(int red, int green, int blue) {
-
-        float min = Math.min(Math.min(red, green), blue);
-        float max = Math.max(Math.max(red, green), blue);
-
-        if (min == max) {
-            return 0;
-        }
-
-        float hue = 0f;
-        if (max == red) {
-            hue = (green - blue) / (max - min);
-
-        } else if (max == green) {
-            hue = 2f + (blue - red) / (max - min);
-
-        } else {
-            hue = 4f + (red - green) / (max - min);
-        }
-
-        hue = hue * 60;
-        if (hue < 0) hue = hue + 360;
-
-        return Math.round(hue);
-    }
-
-    private int get_brightness(int red, int green, int blue) {
-        return (int)(((double)(red + green + blue))/3);
     }
 }
