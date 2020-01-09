bf05ac1008f3ccffd6963b10dbe48e1cebd8db82
robotics <robotics@xaviersaints.org>
2020-01-09 09:36:31 -0600

    Moves around some packages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/dead/OneStoneOld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/dead/OneStoneOld.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.autonomous.dead;
+package org.firstinspires.ftc.teamcode.opmodes.dead;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
